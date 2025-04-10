d4d36a7bd89f257b82b76a14a1aa705e187e82cf
mshrmw <juli12021972@gmail.com>
2025-04-10 12:33:02 +0300

    досылаю тест

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TKVar1;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SucsessTestMethod1()
+        {
+            var win = new MainWindow();
+            Assert.IsTrue(win.Calculate("9", "3", "0", "7"));
+            Assert.IsTrue(win.Calculate("10", "50", "30", "10"));
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TKVar1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void FailtureTestMethod1()
+        {
+            var win = new MainWindow();
+            Assert.IsFalse(win.Calculate("", "", "", ""));
+            Assert.IsFalse(win.Calculate(" ", " ", " ", " "));
+            Assert.IsFalse(win.Calculate("а", "ы", "b", "K"));
+            Assert.IsFalse(win.Calculate("-3", "-4", "-10", "-2"));
+            Assert.IsFalse(win.Calculate("20", "51", "12", "100"));
+
+        }
+    }
+}
